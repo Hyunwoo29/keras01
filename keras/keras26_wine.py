05ffdf57d643a4e361111e1280c8b46821cb439c
Hyunwoo29 <nbaksa3@gmail.com>
2021-07-15 20:29:14 +0900

    20:29

--- keras/keras26_wine.py
+++ keras/keras26_wine.py
@@ -1,45 +1,21 @@
 import numpy as np
 from sklearn import datasets
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_wine
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
-
-datasets = load_iris()
-print(datasets.DESCR)
-print(datasets.feature_names)
+datasets = load_wine()
+# print(datasets.DESCR)
+# print(datasets.feature_names)
 
 x = datasets.data
 y = datasets.target
 
-# print(x.shape, y.shape) # (150, 4) (150,)
-# print(y)
-# [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-#  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2
-#  2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-#  2 2]
-
-# 원핫인코딩 One-Hot-Encoding   (150,) -> (150, 3)
-# 0 -> [1, 0, 0]
-# 1 -> [0, 1, 0]
-# 2 -> [0, 0, 1]
-
-# [0, 1, 2, 1] ->
-# [[1, 0, 0]
-# [0, 1, 0]
-# [0, 0, 1]
-# [0, 1, 0]]   (4,) -> (4, 3)
+print(x.shape, y.shape)
+print(y)
 
 from tensorflow.keras.utils import to_categorical
 y = to_categorical(y)
-print(y[:5])
-# [[1. 0. 0.]
-#  [1. 0. 0.]
-#  [1. 0. 0.]
-#  [1. 0. 0.]
-#  [1. 0. 0.]]
-print(y.shape) # (150, 3)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, shuffle=True, random_state=66)
 
@@ -51,7 +27,7 @@ x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test) 
 
 model = Sequential()
-model.add(Dense(128, activation='relu', input_shape=(4,)))
+model.add(Dense(128, activation='relu', input_shape=(13,)))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
@@ -71,9 +47,12 @@ print('lose : ', loss[0])
 print('accuracy : ', loss[1])
 
 print("=========== 예측 =============")
-print(y_test[:5])
-y_predict = model.predict(x_test[:5])
+print(y_test)
+y_predict = model.predict(x_test)
 print(y_predict)
 
-# r2 = r2_score(y_test, y_predict)
-# print('r2score: ', r2)
+#완성하시오!!
+# acc 0.8 이상 만들것!
+
+# lose :  0.03139385208487511
+# accuracy :  0.9814814925193787
