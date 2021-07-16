de91717c39839aef0f63ecd648d6dba945e80283
Hyunwoo29 <nbaksa3@gmail.com>
2021-07-16 11:24:49 +0900

    hell

---
+++ keras/keras26_wine2.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Input
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+# ./ -> 현재폴더  ../ -> 상위폴더
+datasets = pd.read_csv('./_data/winequality-white.csv', sep=';', index_col=False, header=0)
+datasets = datasets.to_numpy()
+x = datasets[:,0:12]
+y = datasets[:,-1]
+# print(np.unique(y)) # [3. 4. 5. 6. 7. 8. 9.]
+
+
+# print(datasets)
+# print(datasets.shape) # (4898, 12)
+# print(datasets.info())
+# print(datasets.describe())
+
+# 다중분류
+# 모델링하고
+# 0.8 이상 완성!
+# 1. 판다스 -> 넘파이
+# 2. x와  y를 분리
+# 3. sklearn의 onhot 사용할것
+# 4. y의 라벨을 확인 np. unique(y)
+
+
+
+# x = datasets.data
+# y = datasets.target
+
+from tensorflow.keras.utils import to_categorical
+y = to_categorical(y)
+# print(y.shape) #(4898, 10)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, shuffle=True, random_state=66)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test) 
+
+model = Sequential()
+model.add(Dense(128, activation='relu', input_shape=(12,)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(10, activation='softmax')) # 다중분류
+
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy') # 2진 분류에서는 binary_crossentropy 를 쓰면된다.
+
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='loss', patience=5, mode='min', verbose=1)
+
+model.fit(x_train, y_train, epochs=100, batch_size=8, validation_split=0.2, callbacks=[es])
+
+loss = model.evaluate(x_test, y_test)     
+print('lose : ', loss[0])
+print('accuracy : ', loss[1])
+
+print("=========== 예측 =============")
+print(y_test)
+y_predict = model.predict(x_test)
+print(y_predict)
--- keras/practice.py
+++ keras/practice.py
@@ -1,3 +1,7 @@
-import matplotlib as mpl
-
-print(mpl.matplotlib_fname())
+import numpy as np
+from sklearn import datasets
+from sklearn.datasets import load_wine
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+datasets = load_wine()
