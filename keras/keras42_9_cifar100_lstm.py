57905ffeb2cfb9154e25f96c6bf0e8b314612652
Hyunwoo29 <nbaksa3@gamil.com>
2021-07-21 03:25:55 +0900

    lmst

---
+++ keras/keras42_9_cifar100_lstm.py
@@ -0,0 +1,66 @@
+from tensorflow.keras.datasets import cifar100
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 1. data
+(x_train, y_train), (x_test, y_test) = cifar100.load_data() 
+
+x_train = x_train.reshape(50000, 32*32*3) # (50000, 32, 32, 3)
+x_test = x_test.reshape(10000, 32*32*3) # (10000, 32, 32, 3)
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler, QuantileTransformer, PowerTransformer
+scaler = QuantileTransformer()
+# scaler.fit(x_train) 
+# x_train = scaler.transform(x_train) 
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test) 
+
+x_train = x_train.reshape(50000, 32*32, 3)
+x_test = x_test.reshape(10000, 32*32, 3)
+
+from sklearn.preprocessing import OneHotEncoder
+one = OneHotEncoder()
+y_train = y_train.reshape(-1,1)
+y_test = y_test.reshape(-1,1)
+one.fit(y_train)
+y_train = one.transform(y_train).toarray() # (50000, 100)
+y_test = one.transform(y_test).toarray() # (10000, 100)
+
+# 2. model
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, LSTM, Dropout, Input
+
+input1 = Input(shape=(32*32, 3))
+xxx = LSTM(units=20, activation='relu')(input1)
+xxx = Dense(128, activation='relu')(xxx)
+xxx = Dense(64, activation='relu')(xxx)
+xxx = Dense(32, activation='relu')(xxx)
+xxx = Dense(16, activation='relu')(xxx)
+xxx = Dense(8, activation='relu')(xxx)
+output1 = Dense(100, activation='softmax')(xxx)
+
+model = Model(inputs=input1, outputs=output1)
+
+# 3. comple fit // metrics 'acc'
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
+
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='val_loss', patience=20, mode='min', verbose=1)
+
+import time 
+start_time = time.time()
+model.fit(x_train, y_train, epochs=10, batch_size=3000, verbose=2,
+    validation_split=0.025)
+end_time = time.time() - start_time
+
+# 4. predict eval -> no need to
+
+loss = model.evaluate(x_test, y_test)
+print("time : ", end_time)
+print('loss : ', loss[0])
+print('acc : ', loss[1])
+
+# time :  247.91537618637085
+# loss :  nan
+# acc :  0.009999999776482582
