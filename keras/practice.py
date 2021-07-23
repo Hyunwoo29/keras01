7bc8f0b3ccc4785a071e10bda48b667f250d4881
Hyunwoo29 <nbaksa3@gamil.com>
2021-07-23 18:08:51 +0900

    nn

--- keras/practice.py
+++ keras/practice.py
@@ -1,106 +1,128 @@
-from PIL import Image
-import os, glob, numpy as np
+import time
+import datetime
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential, Model,load_model,save_model
-from keras.layers import Input, Activation
-from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization,Activation,ZeroPadding2D,Add
-from keras.layers import GlobalAveragePooling2D
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-import matplotlib.pyplot as plt
-from keras import backend as K
-import tensorflow as tf
-from tensorflow.python.framework import ops as tf_ops
-from keras.optimizers import Adam
-from tensorflow.keras.datasets import cifar100
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer, OneHotEncoder
+from sklearn.metrics import r2_score, mean_squared_error
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.layers import Dense, Input, Conv1D, Conv2D, Flatten, MaxPooling1D, MaxPooling2D, GlobalAveragePooling1D, GlobalAveragePooling2D, Dropout, LSTM, GRU
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
-(x_train,y_train),(x_test,y_test) = cifar100.load_data()
+#1. Data Preprocessing
+# read_csv
+datasets_sk = pd.read_csv('./_data/SK주가 20210721.csv', header=0, usecols=[1,2,3,4,10], nrows=2602, encoding='EUC-KR')
+datasets_samsung = pd.read_csv('./_data/삼성전자 주가 20210721.csv', header=0, usecols=[1,2,3,4,10], nrows=2602, encoding='EUC-KR')
 
-x_train = x_train / 255.
-x_test = x_test / 255.
+# 역순정렬 및 null값 제거
+datasets_sk = datasets_sk[::-1]
+datasets_samsung = datasets_samsung[::-1]
+datasets_sk = datasets_sk.dropna(axis=0)
+datasets_samsung = datasets_samsung.dropna(axis=0)
+# print(datasets_sk.head())
+# print(datasets_samsung.head())
 
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-one = OneHotEncoder()
-y_train = y_train.reshape(-1,1)
-y_test = y_test.reshape(-1,1)
-one.fit(y_train)
-y_train = one.transform(y_train).toarray()
-y_test = one.transform(y_test).toarray()
-print(y_train.shape)            # (50000,10)
-print(y_test.shape)            # (10000,10)
+# 주가4종류 (시가,고가,저가,종가) 와 거래량의 수치차이가 크기때문에 따로 MinMaxScaling - samsung
+data1_ss = datasets_samsung.iloc[:, :-1] # 주가4종 추출
+data2_ss = datasets_samsung.iloc[:, -1:] # 거래량 추출 
+# print(data1_ss.head(), data2_ss.head())
+scaler = MinMaxScaler()
+scaler.fit(data1_ss)
+data1_ss_scaled = scaler.transform(data1_ss) # scaled_ratio, bias를 구하기위해 naming 분리 (data1_ss 원본필요)
+scaler.fit(data2_ss)
+data2_ss = scaler.transform(data2_ss)
+# print(data1_ss_scaled, data2_ss)
+data_ss = np.concatenate((data1_ss_scaled, data2_ss), axis=1) # 병합 (주가4종 오른쪽 열에 거래량 추가)
+# print(data_ss.shape) # (2602, 5)
+scaled_ratio = np.max(data1_ss) - np.min(data1_ss) 
+# print(scaled_ratio[3], np.min(data1_ss) [3]) # 종가에 해당하는 값(4번째 값)
 
-x_train,x_valid, y_train, y_valid = train_test_split(x_train,y_train, train_size = 0.8, shuffle=True,random_state=66)
+# 상동 - sk
+data1_sk = datasets_sk.iloc[:, :-1] 
+data2_sk = datasets_sk.iloc[:, -1:] 
+# print(data1_sk.head(), data2_sk.head())
+scaler = MinMaxScaler()
+scaler.fit(data1_sk)
+data1_sk = scaler.transform(data1_sk)
+scaler.fit(data2_sk)
+data2_sk = scaler.transform(data2_sk)
+# print(data1_sk, data2_sk)
+data_sk = np.concatenate((data1_sk, data2_sk), axis=1) 
+# print(data_sk.shape) # (2602, 5)
 
+'''
+************************Change Here************************
+'''
+target = data_ss[:, [-2]] # 첫번째 target =  '삼성' + '종가' = data_ss + 뒤에서 2번째 열 
+# print(target.shape) # (2602, 1)
+'''
+************************Change Here************************
+'''
 
+# LSTM 처리를 위한 data_split (= split_x)
+x1 = [] # 삼성
+x2 = [] # SK
+y = [] # target
+size = 50 # 데이터 slice 단위일수 설정 (단위일수만큼 끊어서 저장) -> 단위일수만큼의 데이터를 가지고 그 다음날의 주가 예측
+for i in range(len(target) - size + 1):
+    x1.append(data_ss[i: (i + size) ])
+    x2.append(data_sk[i: (i + size) ])
+    y.append(target[i + (size - 1)]) 
+# 설정한 단위일수 만큼 최근 데이터 slice -> y_predict 를 위한 x1_pred, x2_pred 생성
+x1_pred = [data_ss[len(data_ss) - size : ]]
+x2_pred = [data_sk[len(data_ss) - size : ]]
 
+# numpy 배열화
+x1 = np.array(x1)
+x2 = np.array(x2)
+y = np.array(y)
+x1_pred = np.array(x1_pred)
+x2_pred = np.array(x2_pred)
+print(x1.shape, x2.shape, y.shape, x1_pred.shape, x2_pred.shape) # (2553, 50, 5) (2553, 50, 5) (2553, 1) (1, 50, 5) (1, 50, 5)
 
+x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, y, test_size=0.2, random_state=9)
 
-idg = ImageDataGenerator(
-    width_shift_range=(0.1),   #
-    height_shift_range=(0.1),
-    )    
+#2. Modeling
+input1 = Input(shape=(50, 5))
+d1 = LSTM(16, activation='relu')(input1)
 
+input2 = Input(shape=(50, 5))
+d2 = LSTM(16, activation='relu')(input2)
 
-train_generator = idg.flow(x_train,y_train,batch_size=128)
-valid_generator = idg.flow(x_valid,y_valid)
+from tensorflow.keras.layers import concatenate
+m = concatenate([d1, d2])
+d3 = Dense(4, activation='relu')(m)
+output = Dense(1, activation='relu')(d3)
 
-model = Sequential()
-model.add(Conv2D(filters = 16, kernel_size =(1,1), padding = 'valid', strides=(1,1),
-input_shape = (32,32,3)))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Conv2D(filters = 16, kernel_size =(3,3), padding = 'same',strides=(1,1)))
-model.add(BatchNormalization())
-model.add(Activation('relu'))                         
-model.add(Conv2D(filters = 64, kernel_size =(1,1), padding = 'valid',strides=(1,1)))
-model.add(BatchNormalization())
-model.add(Activation('relu')) 
-model.add(MaxPooling2D((3, 3), 2))
+model = Model(inputs=[input1, input2], outputs=output)
 
-model.add(Conv2D(filters = 32, kernel_size =(1,1), padding = 'valid',strides=(1,1)))
-model.add(BatchNormalization())    
-model.add(Activation('relu'))
-model.add(Conv2D(filters = 32, kernel_size =(3,3), padding = 'same',strides=(1,1)))
-model.add(BatchNormalization())
-model.add(Activation('relu'))                              
-model.add(Conv2D(filters = 128, kernel_size =(1,1), padding = 'valid',strides=(1,1)))
-model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(MaxPooling2D((3, 3), 2)) 
+#3. Compiling, Training
+model.compile(loss='mse', optimizer='adam')
 
-model.add(Conv2D(filters = 64, kernel_size =(1,1), padding = 'valid', strides=(1,1)))
-model.add(BatchNormalization())     
-model.add(Activation('relu'))                        
-model.add(Conv2D(filters = 64, kernel_size =(3,3), padding = 'same', strides=(1,1)))
-model.add(BatchNormalization())
-model.add(Activation('relu')) 
-model.add(Conv2D(filters = 256, kernel_size =(1,1), padding = 'valid', strides=(1,1)))
-model.add(BatchNormalization())
-model.add(Activation('relu')) 
-model.add(MaxPooling2D((3, 3), 2))
-model.add(GlobalAveragePooling2D())
-model.add(Dropout(0.1))
-model.add(Dense(128,activation='relu'))
-model.add(Dropout(0.1))
-model.add(Dense(100, activation='softmax'))
-# model2 = load_model('../data/modelcheckpoint/myPproject_5.hdf5')
-model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001,epsilon=None), metrics=['acc'])
-
-model.summary()
-model_path = '../data/modelcheckpoint/my_project_act2.hdf5'
-checkpoint = ModelCheckpoint(filepath=model_path , monitor='val_acc', verbose=1, save_best_only=True)
-early_stopping = EarlyStopping(monitor='val_acc', patience=30)
-lr = ReduceLROnPlateau(patience=15, factor=0.5,verbose=1)
-
-history = model.fit_generator(train_generator,epochs=100, steps_per_epoch= len(x_train) / 128,
-validation_data=valid_generator, callbacks=[early_stopping,lr,checkpoint])
-
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss'] 
-val_loss = history.history['val_loss']
+date = datetime.datetime.now()
 
 
-print('acc : ', acc[-1])
-print('val_acc : ', val_acc[:-1])
+es = EarlyStopping(monitor='val_loss', restore_best_weights=False, mode='auto', verbose=1, patience=4)
+
+
+model.fit([x1_train, x2_train], y_train, epochs=8, batch_size=8, verbose=1, validation_split=0.001, callbacks=[es])
+
+
+#4. Evaluating, Prediction
+loss = model.evaluate([x1_test, x2_test], y_test)
+y_pred = model.predict([x1_pred, x2_pred])
+# y_pred = scaler.inverse_transform(y_pred) # 가격(원) 확인을 위한 inverse scaling
+y_pred = y_pred * scaled_ratio[3] + np.min(data1_ss)[3]
+'''
+scaler.inverse_transform을 할때 data1_ss 의 scaling 정보가 필요
+위에서 scaling을 여러번함 & 특정 scaler.fit 정보만 뽑아 적용하는 방법 모름
+무지성 수식계산으로 땜빵
+-> 사후활용을 위해 scaler를 저장하는 기능 있다함 
+-> import joblib // joblib.dump(scaler, 'scaler.save') // scaler = joblib.load('scaler.save')
+배운것만 쓰라고 해서 안씀
+'''
+
+print('loss = ', loss)
+print("Tomorrow's closing price = ", y_pred)
--- keras/test03.py
+++ keras/test03.py
@@ -9,34 +9,34 @@ from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.layers import Dense, Input, Conv1D, Conv2D, Flatten, MaxPooling1D, MaxPooling2D, GlobalAveragePooling1D, GlobalAveragePooling2D, Dropout, LSTM, GRU
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 #1. 데이터
-datasets = pd.read_csv('./_data/삼성전자 주가 20210721.csv', sep=',', index_col=0, header=0 , encoding='CP949')
-datasets1 = pd.read_csv('./_data/SK주가 20210721.csv', sep=',', index_col=0, header=0, encoding='CP949')
-datasets = datasets.sort_values(by='일자')
-datasets1 = datasets1.sort_values(by='일자')
-datasets.drop(['Unnamed: 15'], axis = 1, inplace = True) # Unnamed: 15 라는 열 제거 
-datasets1.drop(['Unnamed: 15'], axis = 1, inplace = True) # Unnamed: 15 라는 열 제거
-datasets= datasets.dropna(axis=0)
-datasets1= datasets1.dropna(axis=0)
+datasets = pd.read_csv('./_data/삼성전자 주가 20210721.csv', sep=',', index_col=0, header=0 ,nrows=2602, encoding='CP949')
+datasets1 = pd.read_csv('./_data/SK주가 20210721.csv', sep=',', index_col=0, header=0,nrows=2602, encoding='CP949')
+datasets = datasets[['시가','고가','저가','거래량','종가']]
+datasets1 = datasets1[['시가','고가','저가','거래량','종가']]
+
+datasets = datasets.dropna(axis=0)
+datasets1 = datasets.dropna(axis=0)
+
+datasets = datasets.sort_values(by=['일자'], axis=0).values
+datasets1 = datasets1.sort_values(by=['일자'], axis=0).values
 
 # print(datasets) # [3601 rows x 0 columns], [3601 rows x 15 columns] ---> 결측치 제거한후 [3600 rows x 14 columns]
 # print(datasets1) # [3601 rows x 0 columns] , [3601 rows x 15 columns]    -> 결측치 제거한후 [3600 rows x 14 columns]
 # print(datasets.info()) # dtypes: float64(5), object(9)
 # print(datasets.isnull().sum()) # Unnamed: 15    3601
 # print(datasets1.isnull().sum()) # Unnamed: 15    3601
-
-datasets.drop(['종가 단순 5', '10', '20', '60', '120', '단순 5', '20.1', '60.1', '120.1'], axis='columns', inplace=True)
-datasets1.drop(['종가 단순 5', '10', '20', '60', '120', '단순 5', '20.1', '60.1', '120.1'], axis='columns', inplace=True)
-
-scaler = MinMaxScaler()
+datasets = pd.DataFrame(datasets)
+datasets1 = pd.DataFrame(datasets1)
 data1 = datasets.iloc[:, :-1]
 data2 = datasets.iloc[:, -1:]
+scaler = MinMaxScaler()
 data1 = scaler.fit_transform(data1)
 data2 = scaler.fit_transform(data2)
 dataset = np.concatenate((data1, data2), axis=1)
 
-scaler = MinMaxScaler()
 data_1 = datasets1.iloc[:, :-1]
 data_2 = datasets1.iloc[:,-1:]
+scaler = MinMaxScaler()
 data_1 = scaler.fit_transform(data_1)
 data_2 = scaler.fit_transform(data_2)
 dataset1 = np.concatenate((data_1, data_2), axis=1)
