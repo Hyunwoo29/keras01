5b2807e722de12ff5cc6d84c509bdac62585494e
Hyunwoo29 <nbaksa3@gmail.com>
2021-09-10 15:14:21 +0900

    xx

---
+++ teamproject/yolov5(train,val).py
@@ -0,0 +1,17 @@
+# jpg 이미지 저장수
+from glob import glob
+
+img_list = glob('./teamproject/yolov5/movie/images/*.jpg')
+
+# print(len(img_list))
+
+from sklearn.model_selection import train_test_split
+train_img_list, val_img_list = train_test_split(img_list, test_size=0.2, random_state=11)
+
+print(len(train_img_list), len(val_img_list)) 
+
+with open('./teamproject/yolov5/train.txt', 'w') as f:
+    f.write('\n'.join(train_img_list) + '\n')
+
+with open('./teamproject/yolov5/val.txt', 'w') as f:
+    f.write('\n'.join(val_img_list) + '\n')
